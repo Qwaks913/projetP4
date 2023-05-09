1fe58adc4f05251e29c8bdaf0bd7a6c3da53bd0b
natchouppi <132594313+natchouppi@users.noreply.github.com>
2023-05-09 23:28:47 +0200

    Update main.py

--- LaboP4/main.py
+++ LaboP4/main.py
@@ -5,21 +5,21 @@ def main():
     source_path = os.getcwd()
     save_path = os.getcwd()
     if (platform.system() == 'Windows'):
-        files = os.listdir("%s\\mesures" % source_path)
+        files = os.listdir("%s\mesures" % source_path)
     else:
         files = os.listdir("%s/mesures" % source_path)
-    for file in files:
-        if file != ".DS_Store":
-            print(file)
+    for file_name in files:
+        if file_name != ".DS_Store":
+            print("On s'occupe du fichier suivant: " + file_name)
             if (platform.system() == 'Windows'):
-                file2 = "%s\\mesures\\%s" % (source_path, file)  # Windows
+                file_path = "%s\mesures\%s" % (source_path, file_name)  # Windows
             else:
-                file2 = "%s/mesures/%s" % (source_path, file)  # Mac et Linux
+                file_path = "%s/mesures/%s" % (source_path, file_name)  # Mac et Linux
             #If only load = 0 --> Saves graphs and return chirp matrix
             #If only load = 1 --> Doesn't save graphs and return chirp matrix
             #If only load = 2 --> Doesn't save graphs and returns frame matrix
             #If only load = 3 --> Returns fft's matrix
-            fn.fem(file2, file, source_path, only_load=0)
-measure_file = 'GR13_4m_2l.npz'
+            fn.fem(file_path, file_name, source_path, only_load=0)
+measure_file = 'Tracking-40.npz'
 #fn.angle(measure_file)
 main()
